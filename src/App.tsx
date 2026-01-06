088cc2862d509657fc1a1ed320bf021ccaa70d1b
Lucie Seng <lucie.sng@gmail.com>
2026-01-06 17:17:37 +0100

    feat : cta don

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/Homepage";
 import Adopte from "./pages/Adopte";
+import CtaDon from "./components/CtaDon";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/Adopte" element={<Adopte />} />
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/Adopte" element={<Adopte />} />
+        </Routes>
+        <CtaDon />
+      </Router>
+    </div>
   );
 }
 
