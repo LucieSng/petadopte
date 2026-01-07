7e32cba6db847d713d988e07bf0867c99869e530
JeannePeuron <jeanne.peuron@gmail.com>
2026-01-07 17:31:04 +0100

    feat : add image to code + border style

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -4,44 +4,54 @@
 // 3 lignes et 4 colonnes
 export default function ImageGallery() {
   return (
-    <div id="tableau entier" className=" grid grid-cols-4 grid-rows-3 gap-4  ">
-      <div id="Grande Image 1 - Boulldog" className=" bg-amber-900 row-span-2 ">
-        Grande Image
-      </div>
-      <div id="Petite image 1 - Hamster herbe" className="bg-amber-300">
-        Petite image
-      </div>
+    <div
+      id="tableau entier"
+      className=" grid grid-cols-4 grid-rows-3 gap-4 p-3 m-4"
+    >
+      <div
+        id=" 1 - Boulldog"
+        className="row-span-2 h-screen bg-cover bg-center rounded-md "
+        style={{ backgroundImage: "url('/GrandeImage1.jpg')" }}
+      ></div>
+      <div
+        id="Petite image 1 - Hamster herbe"
+        className="bg-cover bg-center rounded-md "
+        style={{ backgroundImage: "url('/PetiteImage1.jpg')" }}
+      ></div>
 
       <div
-        id="Grande Image 2 - Chat bandana jaune"
-        className=" bg-amber-900 row-span-2 "
-      >
-        Grande Image
-      </div>
-      <div id="Petite image 2 - Lapin" className="bg-amber-300">
-        Petite image
-      </div>
+        id=" 2 - Chat bandana jaune"
+        className="row-span-2 h-screen bg-cover bg-center rounded-md "
+        style={{ backgroundImage: "url('/GrandeImage2.jpg')" }}
+      ></div>
+      <div
+        id=" 2 - Lapin"
+        className="bg-cover bg-center rounded-md "
+        style={{ backgroundImage: "url('/PetiteImage2.jpg')" }}
+      ></div>
 
       <div
-        id="Grande Image 3 - Chien lunette"
-        className=" bg-amber-900 row-span-2 "
-      >
-        Grande Image
-      </div>
+        id=" 3 - Chien lunette"
+        className="row-span-2 h-screen bg-cover bg-center rounded-md "
+        style={{ backgroundImage: "url('/GrandeImage3.jpg')" }}
+      ></div>
       <div
-        id="Grande Image 4 - Chien Merlin"
-        className=" bg-amber-900 row-span-2 "
-      >
-        Grande Image
-      </div>
+        id=" 4 - Chien Merlin"
+        className="row-span-2 h-screen bg-cover bg-center rounded-md "
+        style={{ backgroundImage: "url('/GrandeImage4.jpg')" }}
+      ></div>
 
-      <div id="Petite image 3 - Chat noir et blanc" className="bg-amber-300">
-        Petite image
-      </div>
+      <div
+        id=" 3 - Chat noir et blanc"
+        className="bg-cover bg-center rounded-md "
+        style={{ backgroundImage: "url('/PetiteImage3.jpg')" }}
+      ></div>
 
-      <div id="Petite image 4 - Hamster pas herbe" className="bg-amber-300">
-        Petite image
-      </div>
+      <div
+        id=" 4 - Hamster pas herbe"
+        className="bg-cover bg-center rounded-md "
+        style={{ backgroundImage: "url('/PetiteImage4.jpg')" }}
+      ></div>
     </div>
   );
 }
