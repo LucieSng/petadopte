b774b0c45caba5aedcde73cbe43541b0cfbfb3c5
JeannePeuron <jeanne.peuron@gmail.com>
2026-01-07 17:13:54 +0100

    feat : save image Gallery

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -5,32 +5,41 @@
 export default function ImageGallery() {
   return (
     <div id="tableau entier" className=" grid grid-cols-4 grid-rows-3 gap-4  ">
-      <div id="Grande Image" className=" bg-amber-900 row-span-2 ">
+      <div id="Grande Image 1 - Boulldog" className=" bg-amber-900 row-span-2 ">
         Grande Image
       </div>
-      <div id="Petite image" className="bg-amber-300">
+      <div id="Petite image 1 - Hamster herbe" className="bg-amber-300">
         Petite image
       </div>
 
-      <div id="Grande Image" className=" bg-amber-900 row-span-2 ">
+      <div
+        id="Grande Image 2 - Chat bandana jaune"
+        className=" bg-amber-900 row-span-2 "
+      >
         Grande Image
       </div>
-      <div id="Petite image" className="bg-amber-300">
+      <div id="Petite image 2 - Lapin" className="bg-amber-300">
         Petite image
       </div>
 
-      <div id="Grande Image" className=" bg-amber-900 row-span-2 ">
+      <div
+        id="Grande Image 3 - Chien lunette"
+        className=" bg-amber-900 row-span-2 "
+      >
         Grande Image
       </div>
-      <div id="Grande Image" className=" bg-amber-900 row-span-2 ">
+      <div
+        id="Grande Image 4 - Chien Merlin"
+        className=" bg-amber-900 row-span-2 "
+      >
         Grande Image
       </div>
 
-      <div id="Petite image" className="bg-amber-300">
+      <div id="Petite image 3 - Chat noir et blanc" className="bg-amber-300">
         Petite image
       </div>
 
-      <div id="Petite image" className="bg-amber-300">
+      <div id="Petite image 4 - Hamster pas herbe" className="bg-amber-300">
         Petite image
       </div>
     </div>
