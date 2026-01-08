29d8980f919bda99ce788fafaf23cc33cc57b795
JeannePeuron <jeanne.peuron@gmail.com>
2026-01-08 10:26:21 +0100

    feat : text to ImageGallery

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,60 +1,58 @@
-// Nos animaux à adopter
-
-// Découvrez en images tous nos animaux qui attendent une famille aimante. Chaque photo raconte une histoire et un espoire.
-// 3 lignes et 4 colonnes
 export default function ImageGallery() {
   return (
-    <div
-      id="tableau entier"
-      className=" grid grid-cols-4 grid-rows-3 gap-4 p-3 m-4"
-    >
-      <div
-        id=" 1 - Boulldog"
-        className="row-span-2 h-screen bg-cover bg-center rounded-md "
-        style={{ backgroundImage: "url('/GrandeImage1.jpg')" }}
-      ></div>
-      <div
-        id="Petite image 1 - Hamster herbe"
-        className="bg-cover bg-center rounded-md "
-        style={{ backgroundImage: "url('/PetiteImage1.jpg')" }}
-      ></div>
+    <div id="petAdopte">
+      <div id="text" className="text-center">
+        <h1> Nos animaux à adopter</h1>
 
-      <div
-        id=" 2 - Chat bandana jaune"
-        className="row-span-2 h-screen bg-cover bg-center rounded-md "
-        style={{ backgroundImage: "url('/GrandeImage2.jpg')" }}
-      ></div>
-      <div
-        id=" 2 - Lapin"
-        className="bg-cover bg-center rounded-md "
-        style={{ backgroundImage: "url('/PetiteImage2.jpg')" }}
-      ></div>
+        <p className="m-10">
+          Découvrez en images tous nos animaux qui attendent une famille
+          aimante. Chaque photo raconte une histoire et un espoire.
+        </p>
+      </div>
+      <div id="array" className=" grid grid-cols-4 grid-rows-3 gap-4 p-3 m-4">
+        <div
+          className="row-span-2 h-screen bg-cover bg-center rounded-md "
+          style={{ backgroundImage: "url('/GrandeImage1.jpg')" }}
+        ></div>
+        <div
+          className="bg-cover bg-center rounded-md "
+          style={{ backgroundImage: "url('/PetiteImage1.jpg')" }}
+        ></div>
 
-      <div
-        id=" 3 - Chien lunette"
-        className="row-span-2 h-screen bg-cover bg-center rounded-md "
-        style={{ backgroundImage: "url('/GrandeImage3.jpg')" }}
-      ></div>
-      <div
-        id=" 4 - Chien Merlin"
-        className="row-span-2 h-screen bg-cover bg-center rounded-md "
-        style={{ backgroundImage: "url('/GrandeImage4.jpg')" }}
-      ></div>
+        <div
+          className="row-span-2 h-screen bg-cover bg-center rounded-md "
+          style={{ backgroundImage: "url('/GrandeImage2.jpg')" }}
+        ></div>
+        <div
+          className="bg-cover bg-center rounded-md "
+          style={{ backgroundImage: "url('/PetiteImage2.jpg')" }}
+        ></div>
 
-      <div
-        id=" 3 - Chat noir et blanc"
-        className="bg-cover bg-center rounded-md "
-        style={{ backgroundImage: "url('/PetiteImage3.jpg')" }}
-      ></div>
+        <div
+          className="row-span-2 h-screen bg-cover bg-center rounded-md "
+          style={{ backgroundImage: "url('/GrandeImage3.jpg')" }}
+        ></div>
+        <div
+          className="row-span-2 h-screen bg-cover bg-center rounded-md "
+          style={{ backgroundImage: "url('/GrandeImage4.jpg')" }}
+        ></div>
 
-      <div
-        id=" 4 - Hamster pas herbe"
-        className="bg-cover bg-center rounded-md "
-        style={{ backgroundImage: "url('/PetiteImage4.jpg')" }}
-      ></div>
+        <div
+          className="bg-cover bg-center rounded-md "
+          style={{ backgroundImage: "url('/PetiteImage3.jpg')" }}
+        ></div>
+
+        <div
+          className="bg-cover bg-center rounded-md "
+          style={{ backgroundImage: "url('/PetiteImage4.jpg')" }}
+        ></div>
+      </div>
     </div>
   );
 }
 
 // Ajouter le bouton "Voir tous les animaux"
-// Renommer les id
+
+// Possibilité de stocker le className dans une variable pour les petites et grandes images afin de ne pas avoir à le réécrire à chaque fois
+
+// Même chose pour le style qui appelle l'image.
