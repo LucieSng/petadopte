b0bb52c37139d9b82db68df6f628e759583fdb28
Lucie Seng <lucie.sng@gmail.com>
2026-01-09 15:03:22 +0100

    fix : delete useless links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,79 +13,71 @@ const Footer = () => {
                 animal abandonné et de promouvoir le bien-être animal.
               </li>
               <li className="mb-4">
-                <div className="flex mt-4 sm:justify-left md:mt-0 space-x-2 rtl:space-x-reverse">
-                  <a href="#" className="text-body hover:text-heading">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      className="lucide lucide-facebook-icon lucide-facebook"
-                    >
-                      <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
-                    </svg>
-                    <span className="sr-only">Facebook page</span>
-                  </a>
-                  <a href="#" className="text-body hover:text-heading ms-5">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      className="lucide lucide-instagram-icon lucide-instagram"
-                    >
-                      <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
-                      <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
-                      <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
-                    </svg>
-                    <span className="sr-only">Instagram page</span>
-                  </a>
-                  <a href="#" className="text-body hover:text-heading ms-5">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      className="lucide lucide-linkedin-icon lucide-linkedin"
-                    >
-                      <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
-                      <rect width="4" height="12" x="2" y="9" />
-                      <circle cx="4" cy="4" r="2" />
-                    </svg>
-                    <span className="sr-only">Linkedin page</span>
-                  </a>
-                  <a href="#" className="text-body hover:text-heading ms-5">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      className="lucide lucide-twitch-icon lucide-twitch"
-                    >
-                      <path d="M21 2H3v16h5v4l4-4h5l4-4V2zm-10 9V7m5 4V7" />
-                    </svg>
-                    <span className="sr-only">Twitch page</span>
-                  </a>
+                <div className="flex space-x-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="lucide lucide-facebook-icon lucide-facebook"
+                  >
+                    <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+                  </svg>
+                  <span className="sr-only">Facebook page</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="lucide lucide-instagram-icon lucide-instagram"
+                  >
+                    <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+                    <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+                    <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
+                  </svg>
+                  <span className="sr-only">Instagram page</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="lucide lucide-linkedin-icon lucide-linkedin"
+                  >
+                    <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
+                    <rect width="4" height="12" x="2" y="9" />
+                    <circle cx="4" cy="4" r="2" />
+                  </svg>
+                  <span className="sr-only">Linkedin page</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="lucide lucide-twitch-icon lucide-twitch"
+                  >
+                    <path d="M21 2H3v16h5v4l4-4h5l4-4V2zm-10 9V7m5 4V7" />
+                  </svg>
+                  <span className="sr-only">Twitch page</span>
                 </div>
               </li>
             </ul>
@@ -95,26 +87,14 @@ const Footer = () => {
               Informations utiles
             </h2>
             <ul className="text-body font-medium">
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  FAQs
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Conseils d'adoption
-                </a>
-              </li>
+              <li className="mb-4">FAQs</li>
+              <li className="mb-4">Conseils d'adoption</li>
               <li className="mb-4">
                 <a href="#" className="hover:underline">
                   Nous contacter
                 </a>
               </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Mentions légales
-                </a>
-              </li>
+              <li className="mb-4">Mentions légales</li>
             </ul>
           </div>
           <div>
@@ -131,7 +111,7 @@ const Footer = () => {
             </ul>
           </div>
         </div>
-        <hr className="my-6 border-default sm:mx-auto lg:my-8" />
+        <hr className="my-6 border-t sm:mx-auto lg:my-8" />
         <span className="block text-sm text-body sm:text-center pb-8">
           © 2025 Adaopte. Tous droits réservés.
           <p>
