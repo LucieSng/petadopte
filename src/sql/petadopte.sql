167c4637e321b89070258f4169983ac77fc85223
Lucie Seng <lucie.sng@gmail.com>
2026-01-19 09:47:05 +0100

    fix : sql file

--- src/sql/petadopte.sql
+++ src/sql/petadopte.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `pets`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(255) NOT NULL,
-    `type` VARCHAR(100) NOT NULL,
+    `type` BIGINT UNSIGNED NOT NULL,
     `breed` VARCHAR(255) NOT NULL,
     `age` INT NULL,
     `location` VARCHAR(255) NOT NULL,
     `description` TEXT NOT NULL,
     `picture` VARCHAR(255) NOT NULL,
-    `adopted` BOOLEAN NOT NULL DEFAULT 'DEFAULT FALSE',
-    `shelter_id` VARCHAR(255) NOT NULL
+    `adopted` BOOLEAN NOT NULL DEFAULT FALSE,
+    `shelter_id` BIGINT UNSIGNED NOT NULL
 );
 CREATE TABLE `volunteers`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -16,7 +16,7 @@ CREATE TABLE `volunteers`(
     `motivations` TEXT NOT NULL
 );
 CREATE TABLE `pet_volunteers`(
-    `id` BIGINT NOT NULL,
+    `id` BIGINT UNSIGNED NOT NULL,
     `pet_id` BIGINT UNSIGNED NOT NULL,
     `volunteer_id` BIGINT UNSIGNED NOT NULL,
     PRIMARY KEY(`id`)
@@ -25,10 +25,10 @@ CREATE TABLE `donators`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `civility` ENUM('M', 'Mme', 'Autre') NOT NULL,
     `address` VARCHAR(255) NOT NULL,
-    `payment_method` ENUM('') NOT NULL,
+    `payment_method` ENUM('carte_bancaire', 'virement', 'cheque', 'paypal') NOT NULL,
     `email` VARCHAR(255) NOT NULL,
     `sponsor` BOOLEAN NOT NULL,
-    `pet_id` BIGINT NOT NULL
+    `pet_id` BIGINT UNSIGNED NOT NULL
 );
 CREATE TABLE `type`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -43,12 +43,16 @@ CREATE TABLE `shelter`(
 );
 CREATE TABLE `shelters_volunteers`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `shelter_id` BIGINT NOT NULL,
-    `volonteer_id` BIGINT NOT NULL,
-    `role` ENUM('') NOT NULL
+    `shelter_id` BIGINT UNSIGNED NOT NULL,
+    `volonteer_id` BIGINT UNSIGNED NOT NULL,
+    `role` ENUM('benevole', 'soigneur', 'enqueteur') NOT NULL
 );
+
+/* Permet de modifier une table existante */
+
 ALTER TABLE
     `shelters_volunteers` ADD CONSTRAINT `shelters_volunteers_shelter_id_foreign` FOREIGN KEY(`shelter_id`) REFERENCES `shelter`(`id`);
+/* La colonne pet_id dans donartors doit correspondre à un id existant dans la table pets  */
 ALTER TABLE
     `donators` ADD CONSTRAINT `donators_pet_id_foreign` FOREIGN KEY(`pet_id`) REFERENCES `pets`(`id`);
 ALTER TABLE
